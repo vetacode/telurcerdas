eb4a954ce66eda6b6fa502465b09f5001f5d82ef
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-06 13:51:12 +0700

    copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
     <>
       <footer className='bottom-0 mt-12 bg-white py-6 text-center shadow-md'>
         <small className='copyright text-gray-600'>
-          © {new Date().getFullYear()} Copyright • Telur Cerdas • Otak Kuat
-          Anak Hebat • All Rights Reserved
+          © {new Date().getFullYear()} Copyright • PT. Diamant Origo
+          International • Telur Cerdas • All Rights Reserved
         </small>
       </footer>
       <motion.a
