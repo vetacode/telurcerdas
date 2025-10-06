ff613eed2cbf46ec0c39fd84e951e32e11ebfa71
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-06 15:02:02 +0700

    footer px-6

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <>
-      <footer className='bottom-0 mt-12 bg-white py-6 text-center shadow-md'>
+      <footer className='bottom-0 mt-12 bg-white px-8 py-6 text-center shadow-md'>
         <small className='copyright text-gray-600'>
           © {new Date().getFullYear()} Copyright • PT. Diamant Origo
           International • Telur Cerdas • All Rights Reserved
