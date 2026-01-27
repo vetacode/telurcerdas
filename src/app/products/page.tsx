c00e453bcd142dea530e5ca44f2a63e1fd3a094e
vetacode <vetacode@gmail.com>
2026-01-27 12:40:51 +0700

    changes kemasan table eggs to partai

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ const products = [
     id: 'egg-premium',
     name: 'Premium Brown Eggs (1 Kg)',
     desc: 'Table Eggs, Telur Pilihan, Kualitas Terbaik',
-    kemasan: 'Minimum packaging',
+    kemasan: 'Partai',
     price: 25800,
     image:
       'https://images.unsplash.com/photo-1506976785307-8732e854ad03?q=80&w=1043&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
